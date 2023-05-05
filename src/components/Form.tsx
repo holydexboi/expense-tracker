10fabb505f6f282cf73b0983b4d829db7d7dabbb
holydexboi <holydexboi@yahoo.com>
2023-05-05 02:58:56 +0100

    validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,51 @@
 import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+
+
+const schema = z.object({
+    description: z.string().min(3, {message: 'Description should be at least 3 characters'}),
+    amount: z.number({invalid_type_error: 'Amount is required'}),
+    category: z.string({required_error: "Category is required"}).min(1)
+})
+
+type FormData = z.infer<typeof schema>
 
 const Form = () => {
+
+    
+    const { register, handleSubmit, formState: { errors },
+} = useForm<FormData>({resolver: zodResolver(schema)})
+
+const onSubmit = (data: FormData) => console.log(data) 
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
           <label htmlFor="description" className="form-label">
             Description
           </label>
-          <input type="text" id="description" className="form-control" />
+          <input {...register("description")} type="text" id="description" className="form-control" />
+          {errors.description && (<p  className="text-danger">{errors.description.message}</p>)}
         </div>
         <div className="mb-3">
           <label htmlFor="amount" className="form-label">
             Amount
           </label>
-          <input id="amount" type="number" className="form-control" />
+        <input {...register("amount", {valueAsNumber: true})} id="amount" type="number" className="form-control" />
+          {errors.amount && (<p  className="text-danger">{errors.amount.message}</p>)}
         </div>
         <div className="mb-3">
             <label htmlFor="category" className="form-label"></label>
-            <select name="" id="category" className="form-select">
-                <option value=""></option>
-                <option value="groceries">Groceries</option>
+            <select {...register("category")}  name="" id="category" className="form-select">
+                
+                <option selected value="groceries">Groceries</option>
                 <option value="utilities">Utilities</option>
                 <option value="entertainment">Entertainment</option>
             </select>
+            {errors.category && (<p  className="text-danger">{errors.category.message}</p>)}
         </div>
         <button className="btn btn-primary" type="submit">Submit</button>
       </form>
